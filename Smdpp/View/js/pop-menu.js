b798e0ff6c848b370535a3627c83a38f66f251c9
panalgin <daflan.official@gmail.com>
2018-02-17 22:59:16 +0300

    Pop menus are nearly ready.

---
+++ Smdpp/View/js/pop-menu.js
@@ -0,0 +1,57 @@
+﻿$(document).ready(function() {
+	$("body").on("click", "div.wrapper ul#nav li", function(e) {
+		var menuId = $(this).attr("id");
+		showMenu(menuId);
+
+		e.stopPropagation();
+	});
+
+	$("body").on("mouseenter", "div.wrapper ul#nav li", function(e) {
+		if (isAnyPopOpen()) {
+			var menuId = $(this).attr("id");
+			var popId = $("ul.pop-menu").filter(":visible").attr("id");
+
+			if (menuId.replace("id", "") != popId.replace("id", "")) {
+				showMenu(menuId);
+			}
+		}
+	});
+
+	$(document).on("click", "body", function(e) {
+		if (isAnyPopOpen()) {
+			closeAllPops();
+		}
+	});
+
+	$("body").on("click", "ul.pop-menu", function(e) {
+	
+		e.stopPropagation(); 
+	});
+});
+
+function isAnyPopOpen() {
+	var pop = $("ul.pop-menu").filter(":visible");
+
+	console.log(pop);
+
+	return pop.length > 0;
+}
+
+function closeAllPops() {
+	$("ul#nav .active").removeClass("active");
+	var pop = $("ul.pop-menu").filter(":visible").hide();
+}
+
+function showMenu(menuId) {
+	var menuItem = $("li#" + menuId);
+	var popId = menuId.replace("menu", "pop");
+	var offset = menuItem.offset();
+
+	offset.top = menuItem.height();
+
+	closeAllPops();
+
+	menuItem.addClass("active");
+	$("ul#" + popId).css("top", offset.top).css("left", offset.left).show();
+}
+
